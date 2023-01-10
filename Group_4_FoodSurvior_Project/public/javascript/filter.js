b3ea7332b61de4c3aedccc6048201d6dae7f8cfd
何德科 <hedeke@hedekedembp.mynetworksettings.com>
2023-01-10 17:05:02 -0500

    first commit

--- Group_4_FoodSurvior_Project/public/javascript/filter.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function (event) {
-    $('#serviceid').change(function () {
-        $this = $(this);
-        $('.card-deck').hide();
-        $('.' + $this.val()).show();
-        console.log("showing " + $this.val() + " boxes");
-    });
-});
